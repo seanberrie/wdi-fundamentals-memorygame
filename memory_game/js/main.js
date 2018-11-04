c464609df9c36b84272e5468bb3525406b790dac
Sean Berrie <thebear@Seans-MBP-2.socal.rr.com>
2018-11-04 09:00:04 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+var cards = ["king", "queen", "king", "queen"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+cardsInPlay.push(cardOne);
+
+console.log("user flipped" + ' ' + cardOne)
+
+if ((cardsInPlay.length === 2) && (cardsInPlay[cardOne] === cardsInPlay[cardTwo])){alert("You Found A Match");}
+else {alert("Sorry Try Again");}
+
+console.log(cardsInPlay.length);
